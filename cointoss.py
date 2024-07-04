ba4c1bd4c6e6bf2da950002ef48d0353bebd0e46
gikyoku <136043781+gikyoku@users.noreply.github.com>
2024-07-04 09:17:29 +0900

    fixed

--- cointoss.py
+++ cointoss.py
@@ -1,10 +1,13 @@
 import random
-
+user = input("Who are you?\n>")
+print("Hello, " + user + "!")
 print("Tossing a coin...")
-
+headcount = 0
 for i in range(3):
     randomNumber = random.randint(1, 2)
     if randomNumber == 1:
         print("Round " + str(i+1) + ": Head")
+        headcount = headcount + 1
     else:
         print("Round " + str(i+1) + ": Tail")
+print("Heads: " + str(headcount) + ", Tails: " + str(3 - headcount))
